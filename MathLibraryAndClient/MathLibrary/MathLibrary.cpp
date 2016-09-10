83098ad12a9e5e27e98f3651e12c34ae68d5945b
MAHA06 <mihai_mahalean@yahoo.com>
2016-09-10 14:31:14 +0300

    IMAGE TO BOOLEAN ARRAY
    finished converting the image to a 2d array of 0 and 1

---
+++ MathLibraryAndClient/MathLibrary/MathLibrary.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "MathLibrary.h"
+
+namespace MathLibrary
+{
+	double Functions::Add(double a, double b)
+	{
+		return a + b;
+	}
+
+	double Functions::Multiply(double a, double b)
+	{
+		return a * b;
+	}
+
+	double Functions::AddMultiply(double a, double b)
+	{
+		return a + (a * b);
+	}
+}
